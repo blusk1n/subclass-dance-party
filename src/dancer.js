4ccd0c84ea7ce5033808c88bcfdf4d7f38f6e43d
blusk1n <tahaznazen@gmail.com>
2019-11-26 22:41:12 +0100

    dancing club

--- src/dancer.js
+++ src/dancer.js
@@ -1,32 +1,61 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps) {
-
-  var dancer = {};
-
+var Dancer = function(top, left, timeBetweenSteps) {
+  this.top = top;
+  this.left = left;
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  this.$node = $('<span class="dancer"></span>');
+  this.timeBetweenSteps = timeBetweenSteps;
+  this.step();
+  this.setPosition(top, left);
 
-  dancer.step = function() {
+}
+
+
+ Dancer.prototype.step = function() {
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
+    
+    
+    setTimeout( this.step.bind(this), this.timeBetweenSteps);
+    // setTimeout(function(){
+    //   console.log(that)
+    //   that.step
+    // },that.timeBetweenSteps)
 
-  dancer.setPosition = function(top, left) {
+
+  };
+
+ Dancer.prototype.setPosition = function(top, left) {
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
     var styleSettings = {
-      top: top,
+      top: 500,
       left: left
     };
-    dancer.$node.css(styleSettings);
+    this.$node.css(styleSettings);
   };
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
 
-  return dancer;
-};
+
+
+
+//   function makeDancer(top, left, timeBetweenSteps) {
+//   this.$node = $('<span class="dancer"></span>');
+//   this.timeBetweenSteps = timeBetweenSteps;
+//    this.step()
+
+ 
+// }; 
+// makeDancer.prototype.step = function(){
+//   var that = this;
+//   console.log(that)
+//   setTimeout(that.step, this.timeBetweenSteps)
+// }
+
+// makeDancer.prototype.setPosition = function(){
+
+// }
+//  
